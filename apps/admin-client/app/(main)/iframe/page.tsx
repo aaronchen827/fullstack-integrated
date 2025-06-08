972b4504158accf47e7fc45a61af3b547542b87c
Aaron <aaronchen827@gamil.com>
2025-06-08 12:50:10 +0800

    add dynamic

--- apps/admin-client/app/(main)/iframe/page.tsx
+++ apps/admin-client/app/(main)/iframe/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 import { useSearchParams } from 'next/navigation'
+import dynamic from 'next/dynamic'
 
-export const dynamic = 'force-dynamic'
-
-export default function IframePage() {
+function IframePage() {
   const searchParams = useSearchParams()
   const iframeUrl = searchParams.get('iframeUrl') || 'https://www.google.com.hk/'
   console.log('iframeUrl=', iframeUrl)
   return <iframe src={iframeUrl} className="w-full h-full border-none" />
 }
+
+export default dynamic(() => Promise.resolve(IframePage), {
+  ssr: false,
+})
