12c9bd6e1092b349531ad6692e7355d59b405782
Aaron <aaronchen827@gamil.com>
2025-06-08 12:40:17 +0800

    fix iframe build error

--- apps/admin-client/app/(main)/iframe/page.tsx
+++ apps/admin-client/app/(main)/iframe/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 import { useSearchParams } from 'next/navigation'
 
+export const dynamic = 'force-dynamic'
+
 export default function IframePage() {
   const searchParams = useSearchParams()
-  const iframeUrl = searchParams.get('iframeUrl') || 'https://www.baidu.com'
+  const iframeUrl = searchParams.get('iframeUrl') || 'https://www.google.com.hk/'
   console.log('iframeUrl=', iframeUrl)
   return <iframe src={iframeUrl} className="w-full h-full border-none" />
 }
